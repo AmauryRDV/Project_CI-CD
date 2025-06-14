249ffe57119f0857ec8854402125a1a56f34ff70
AmauryRDV <97154136+AmauryRDV@users.noreply.github.com>
2025-06-14 15:48:39 +0200

    feat

--- api/index.js
+++ api/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const prometheusMiddleware = require('express-prometheus-middleware');
 const app = express();
 const port = 3000;
+
+app.use(prometheusMiddleware({
+  metricsPath: '/metrics',
+  collectDefaultMetrics: true,
+  requestDurationBuckets: [0.1, 0.5, 1, 1.5],
+}));
 
 const sensors = [
   { id: 1, name: 'Température', value: 22.4, unit: '°C' },
@@ -8,16 +15,13 @@ const sensors = [
   { id: 3, name: 'CO2', value: 415, unit: 'ppm' }
 ];
 
-
 app.get('/', (req, res) => {
   res.send('API de test déployée avec succès');
 });
 
-
 app.get('/sensors', (req, res) => {
   res.json(sensors);
 });
-
 
 app.get('/sensors/:id', (req, res) => {
   const sensorId = parseInt(req.params.id);
